f2c39923d39e336ac0ce3d6f3909fc2983bd5ef9
Gregory Barello <gbarello@ahmadianlabcomp.uoregon.edu>
2019-01-08 12:24:33 -0800

    setup README and setup script. Modified ipynb.

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-conda create -n barello_TF_workshop python=3 tensorflow-gpu numpy matplotlib jupyter
+conda create -n barello_TF_workshop python=3.5.4
+
+source activate barello_TF_workshop
+
+conda install gcc
+conda install numpy
+pip install tensorflow-gpu==1.7.0
+conda install matplotlib
+conda install -c conda-forge jupyterlab
+
+source deactivate
+echo 'Done with setup'
