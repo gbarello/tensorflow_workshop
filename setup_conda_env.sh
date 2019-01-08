99efb29e8b7ce8e8f518923603e0c4ace63bb16c
Gregory Barello <gbarello@ahmadianlabcomp.uoregon.edu>
2019-01-08 12:38:08 -0800

    removed gcc installation

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -4,11 +4,10 @@ conda create -n barello_TF_workshop python=3.5.4
 
 source activate barello_TF_workshop
 
-conda install gcc
 conda install numpy
 pip install tensorflow-gpu==1.7.0
-conda install matplotlib
 conda install -c conda-forge jupyterlab
+conda install matplotlib
 
 source deactivate
 echo 'Done with setup'
