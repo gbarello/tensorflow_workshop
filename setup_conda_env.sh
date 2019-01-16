f653a698c5deeaa608b1e1b3f543fc862a458ccf
Gregory Barello <gbarello@ahmadianlabcomp.uoregon.edu>
2019-01-16 11:15:23 -0800

    removed tensorflow-gpu which allows multiple sessions to be started (I think)

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -5,7 +5,7 @@ conda create -n barello_TF_workshop python=3.5.4
 source activate barello_TF_workshop
 
 conda install numpy
-pip install tensorflow-gpu==1.7.0
+pip install tensorflow==1.7.0
 conda install -c conda-forge jupyterlab
 conda install matplotlib
 
